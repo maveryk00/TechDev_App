df0e9e5cc24e44152aace8ca2c156db98d5ee665
Steven <martin170520@gmail.com>
2023-05-16 12:37:41 -0500

    agregado person y customer

--- src/main/java/com/techdevs/Customer.java
+++ src/main/java/com/techdevs/Customer.java
@@ -1,2 +1,22 @@
-package com.techdevs;public class Customer {
+package com.techdevs;
+
+public class Customer {
+    public int personId,points;
+    public enum Category;
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
