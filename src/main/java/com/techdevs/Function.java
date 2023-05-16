2e6c4c4c86edfbd238000de626aa97a1f78f6897
Daker515 <erosjejexd3@gmail.com>
2023-05-16 14:53:22 +0200

    modificado

--- src/main/java/com/techdevs/Function.java
+++ src/main/java/com/techdevs/Function.java
@@ -1,2 +1,45 @@
-package com.techdevs;public class Function {
+package com.techdevs;
+
+import java.util.Date;
+
+public class Function {
+
+    private int id;
+
+    private Date datetime;
+
+    private float price;
+
+    private int movieId;
+
+    //Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getdatetime() {
+        return datetime;
+    }
+
+    public Date setdatetime(Date datetime) {
+        this.datetime = datetime;}
+
+public float getPrice(){ return  price;
 }
+}
+public void setPrice(float price) {
+    this.price = price;
+}
+public int getMovieId(){return movieId;}
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+}
+
+
+
