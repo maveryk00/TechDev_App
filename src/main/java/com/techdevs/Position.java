77c1f648056f647f4744bc1dd35a00b997a64d27
leonardo dorado <SanchezDLeonardOD@gmail.com>
2023-05-16 19:19:42 -0500

    Creación JavaClass Employee-Position

---
+++ src/main/java/com/techdevs/Position.java
@@ -0,0 +1,25 @@
+package com.techdevs;
+
+public class Position extends Employee{
+
+    private int idPosition;
+    private String namePosition;
+
+
+    //Getters and setters
+    public int getIdPosition() {
+        return idPosition;
+    }
+
+    public void setIdPosition(int idPosition) {
+        this.idPosition = idPosition;
+    }
+
+    public String getNamePosition() {
+        return namePosition;
+    }
+
+    public void setNamePosition(String namePosition) {
+        this.namePosition = namePosition;
+    }
+}
