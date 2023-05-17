39450ff8bff97eb4b1a5237acee1025c99425e30
Felipe Jimenez <maveryk00@gmail.com>
2023-05-16 22:24:26 -0500

    add conection sample

--- src/main/java/com/techdevs/Main.java
+++ src/main/java/com/techdevs/Main.java
@@ -5,7 +5,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        ResultSet resultSet = DatabaseConnection.getAllTable("person");
+        while (resultSet.next()){
+            String name = resultSet.getString(1);
+            String lastName = resultSet.getString(2);
+
+            System.out.println(name + " " + lastName);
+        }
     }
 }
