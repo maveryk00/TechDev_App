fe72996e28e8ced31a9375158f935fefa7978a0a
elbichoyt <pierocarrillomalla35@gmail.com>
2023-05-29 17:43:53 -0500

    Customer y Person lombok

--- src/main/java/com/techdevs/Customer.java
+++ src/main/java/com/techdevs/Customer.java
@@ -1,23 +1,24 @@
 package com.techdevs;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+
 public class Customer extends Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private int personId;
     public int points;
     public String Category;
 
 
-    public int getPoints() {
-        return points;
-    }
 
-    public void setPoints(int points) {
-        this.points = points;
-    }
-
-    public String getCategory() {
-        return Category;
-    }
-
-    public void setCategory(String category) {
-        Category = category;
-    }
 }
--- src/main/java/com/techdevs/Person.java
+++ src/main/java/com/techdevs/Person.java
@@ -1,46 +1,24 @@
 package com.techdevs;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "person")
+
 public class Person {
-    private int id, addressId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+    private int addressId;
     private String name, lastname, email;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(int addressId) {
-        this.addressId = addressId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
